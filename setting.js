0b0abf97c32ad90bc7c277031892a57d8319482b
Liren <liren90@gmail.com>
2019-07-31 19:52:42 +0800

    Setting tweak

---
+++ setting.js
@@ -0,0 +1,22 @@
+const TEAM_SIZE = 2
+
+const NAME_LIST = [
+  'ruby',
+  'raymond',
+  'shawn',
+  'choorhong',
+  'wernsheng',
+  'amar',
+  'henderson',
+  'gab',
+  'pihan',
+  'suzen',
+  'ling',
+  'weihan',
+  'terrence',
+  'cheehaw',
+  'luke',
+  'sunny',
+  'hang',
+  'azran'
+]
