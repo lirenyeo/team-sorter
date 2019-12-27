bfb166b3937c26facffc70be52c8e1c6ecf4d12d
Liren Yeo <liren90@gmail.com>
2019-12-27 11:27:59 +0800

    title and challenges settings

--- setting.js
+++ setting.js
@@ -1,22 +1,26 @@
+const TITLE = 'React To Do List'
+
 const TEAM_SIZE = 2
 
+const CHALLENGES = [1, 2, 4]
+
 const NAME_LIST = [
-  'ruby',
-  'raymond',
-  'shawn',
-  'choorhong',
-  'wernsheng',
-  'amar',
-  'henderson',
-  'gab',
-  'pihan',
-  'suzen',
-  'ling',
-  'weihan',
-  'terrence',
-  'cheehaw',
-  'luke',
-  'sunny',
-  'hang',
-  'azran'
+  'Dan',
+  'Ian',
+  'Ben',
+  'Ger',
+  'Dixon',
+  'Faiz',
+  'Louis',
+  'Andrew',
+  'Min',
+  'Shafiq',
+  'Nazrin',
+  'Sebastien',
+  'Christopher',
+  'Ethan',
+  'Daniel',
+  'Calix',
+  // 'Stephen',
+  'Michael'
 ]
