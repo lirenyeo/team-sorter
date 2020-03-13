45f808b21ab5b3f4067e1001b229f4d67458ce8d
Liren Yeo <liren90@gmail.com>
2020-03-13 21:36:19 +0800

    beer pong

--- setting.js
+++ setting.js
@@ -1,26 +1,18 @@
-const TITLE = 'React To Do List'
+const TITLE = 'BEER PONG CHAMPIONSHIP'
 
 const TEAM_SIZE = 2
 
 const CHALLENGES = [1, 2, 4]
 
 const NAME_LIST = [
-  'Dan',
-  'Ian',
-  'Ben',
-  'Ger',
-  'Dixon',
-  'Faiz',
-  'Louis',
-  'Andrew',
-  'Min',
-  'Shafiq',
-  'Nazrin',
-  'Sebastien',
-  'Christopher',
-  'Ethan',
-  'Daniel',
-  'Calix',
-  // 'Stephen',
-  'Michael'
+  'sandra',
+  'ruey',
+  'faiz',
+  'van',
+  'peony',
+  'ian',
+  'ian',
+  'liren',
+  'nic',
+  'ann'
 ]
